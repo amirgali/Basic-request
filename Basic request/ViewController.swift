7095e673540775492da7ad1fb6cbc80501e48b76
Amirgali <aturalinov@yandex.ru>
2022-12-05 14:54:24 +0500

    Initial Commit

---
+++ Basic request/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Basic request
+//
+//  Created by Амиргали Туралинов on 05.12.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
